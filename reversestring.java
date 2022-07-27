6f004742b023283c3b4e9f0c9ee0f09742abe0dd
omkar bhosale <62058242+omey-bhosale@users.noreply.github.com>
2022-07-27 22:58:22 +0530

    reversestring.java
    Reverse words in a given string

---
+++ reversestring.java
@@ -0,0 +1,80 @@
+Reverse words in a given string
+
+
+
+Given a String S, reverse the string without reversing its individual words. Words are separated by dots.
+
+Example 1:
+
+Input:
+S = i.like.this.program.very.much
+Output: much.very.program.this.like.i
+Explanation: After reversing the whole
+string(not individual words), the input
+string becomes
+much.very.program.this.like.i
+Example 2:
+
+Input:
+S = pqr.mno
+Output: mno.pqr
+Explanation: After reversing the whole
+string , the input string becomes
+mno.pqr
+
+Your Task:
+You dont need to read input or print anything. Complete the function reverseWords() which takes string S as input parameter and returns a string containing the words in reversed order. Each word in the returning string should also be separated by '.' 
+
+
+Expected Time Complexity: O(|S|)
+Expected Auxiliary Space: O(|S|)
+
+
+Constraints:
+1 <= |S| <= 2000
+
+
+
+
+
+
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            String s = sc.next();
+            Solution obj = new Solution();
+            System.out.println(obj.reverseWords(s));
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+
+class Solution 
+{
+    //Function to reverse words in a given string.
+    String reverseWords(String S)
+    {
+        // code here 
+       String [] words=S.split("[.]");
+       String ans="";
+       for(int i=words.length-1;i>=0;--i)
+       {
+           ans=ans+words[i]+".";
+           
+       }
+       return ans.substring(0,ans.length()-1);
+        
+    }
+}
