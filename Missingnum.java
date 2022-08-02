5f52c4675251edc8fc567c51cc40d1e25dc655f5
omkar bhosale <omkarbhosale.officials@gmail.com>
2022-08-02 13:57:06 +0530

    Missing number in array
    Missing number in array

---
+++ Missingnum.java
@@ -0,0 +1,88 @@
+/*
+Missing number in array
+EasyAccuracy: 42.51%Submissions: 100k+Points: 2
+Lamp
+This problem is part of GFG SDE Sheet. Click here to view more.  
+
+Given an array of size N-1 such that it only contains distinct integers in the range of 1 to N. Find the missing element.
+
+Example 1:
+
+Input:
+N = 5
+A[] = {1,2,3,5}
+Output: 4
+Example 2:
+
+Input:
+N = 10
+A[] = {6,1,2,8,3,4,7,10,5}
+Output: 9
+
+Your Task :
+You don't need to read input or print anything. Complete the function MissingNumber() that takes array and N as input  parameters and returns the value of the missing number.
+
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(1)
+
+
+Constraints:
+1 ≤ N ≤ 106
+1 ≤ A[i] ≤ 106
+
+
+
+*/
+
+
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] str = br.readLine().trim().split(" ");
+            int[] array = new int[n - 1];
+            for (int i = 0; i < n - 1; i++) {
+                array[i] = Integer.parseInt(str[i]);
+            }
+            Solution sln = new Solution();
+            System.out.println(sln.MissingNumber(array, n));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int MissingNumber(int array[], int n) {
+        // Your Code Here
+        Arrays.sort(array);
+        int num=1;
+        int i;
+        for(i=0;i<n-1;i++)
+        {
+            if(num==array[i])
+            {
+                num++;
+            }
+            else
+            {
+                return num;
+            }
+        }
+        return num;
+        
+    }
+}
