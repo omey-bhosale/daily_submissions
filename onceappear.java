e351d79f575eb33056436e6c4011ab2e3f95ff7d
omkar bhosale <omkarbhosale.officials@gmail.com>
2022-08-07 00:16:41 +0530

    element that appears once

---
+++ onceappear.java
@@ -0,0 +1,86 @@
+/*
+Find the element that appears once
+MediumAccuracy: 55.91%Submissions: 22289Points: 4
+Given a sorted array A[] of N positive integers having all the numbers occurring exactly twice, except for one number which will occur only once. Find the number occurring only once.
+
+Example 1:
+
+Input:
+N = 5
+A = {1, 1, 2, 5, 5}
+Output: 2
+Explanation: 
+Since 2 occurs once, while
+other numbers occur twice, 
+2 is the answer.
+Example 2:
+
+Input:
+N = 7
+A = {2, 2, 5, 5, 20, 30, 30}
+Output: 20
+Explanation:
+Since 20 occurs once, while
+other numbers occur twice, 
+20 is the answer.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function search() which takes two arguments(array A and integer N) and returns the number occurring only once.
+
+Expected Time Complexity: O(Log(N)).
+Expected Auxiliary Space: O(1).
+
+Constraints
+0 <   N  <= 10^6
+0 <= A[i] <= 10^9
+*/
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main (String[] args)
+    {
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            
+            for (int i = 0; i < n; ++i)
+                arr[i] = sc.nextInt ();
+
+    		System.out.println (new Sol().search (arr, n));
+        }
+        
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Sol
+{
+    public static int search(int A[], int N)
+    {
+        // your code here
+      for(int i=0;i<N-1;i++)
+      {
+          if(A[i]==A[i+1])
+          {
+              i+=1;
+          }else
+          {
+              return A[i];
+          }
+      }
+      return A[N-1];
+    }
+}
+
