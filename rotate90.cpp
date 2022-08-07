155ad1e29854c242fe2c4a5bcced0fa10b234ccb
omkar bhosale <omkarbhosale.officials@gmail.com>
2022-08-07 23:25:34 +0530

    Rotate by 90 degree

---
+++ rotate90.cpp
@@ -0,0 +1,88 @@
+/*
+Rotate by 90 degree
+MediumAccuracy: 53.41%Submissions: 25987Points: 4
+Given a square matrix[][] of size N x N. The task is to rotate it by 90 degrees in an anti-clockwise direction without using any extra space.
+
+Example 1:
+
+Input:
+N = 3
+matrix[][] = [[1 2 3],
+              [4 5 6],
+              [7 8 9]]
+Output:
+3 6 9 
+2 5 8 
+1 4 7
+Your Task:
+You only need to implement the given function rotate(). Do not read input, instead use the arguments given in the function. 
+
+Expected Time Complexity: O(N*N)
+Expected Auxiliary Space: O(1)
+
+Constraints:
+1 ≤ N ≤ 50
+1 <= matrix[][] <= 100
+*/
+
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+void rotate (vector<vector<int> >& matrix);
+
+
+// } Driver Code Ends
+//User function template for C++
+
+/* matrix : given input matrix, you are require 
+ to change it in place without using extra space */
+void rotate(vector<vector<int> >& matrix)
+{
+    // Your code goes here
+    for(int i=0;i<matrix.size();i++)
+    {
+        reverse(matrix[i].begin(),matrix[i].end());
+    }
+    int n=matrix.size();
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i;j<n;j++)
+        {
+            swap(matrix[i][j],matrix[j][i]);
+        }
+    }
+}
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t; 
+    while(t--)
+    {
+        int n;
+        cin>>n; 
+        vector<vector<int> > matrix(n);
+        for(int i=0; i<n; i++)
+        {
+            matrix[i].resize(n);
+            for(int j=0; j<n; j++)
+                cin>>matrix[i][j]; 
+        }
+        rotate(matrix);
+        for (int i = 0; i < n; ++i)
+        {
+            for(int j=0; j<n; j++)
+                cout<<matrix[i][j]<<" ";
+            cout<<"\n";
+        }
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
